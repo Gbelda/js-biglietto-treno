931c5f6efc8975e64d87517335880694a5398946
gbelda <gbelda56@gmail.com>
2021-10-11 15:27:31 +0200

    Complete JS calc values
    rimane da sistemare la parte dell HTML per mostrare il prezzo finale

--- main.js
+++ main.js
@@ -6,22 +6,21 @@ const userAge = parseInt(prompt("Quanti anni ha il passegero?"));
 
 //calcolare prezzo del viaggio (0.21 € al km)
 const grossPrice = kilometres * 0.21
-document.getElementById("initialprice").innerHTML = grossPrice
+document.getElementById("initialprice").innerHTML = "\u20AC" + grossPrice
 
 //applicare lo sconto in base all 'eta (20% minorenni, 40% over 65)
 if (userAge < 18) {
     const minorDiscount = ((grossPrice * 20) / 100).toFixed(2);
-    document.getElementById("discount").innerHTML = minorDiscount
-    document.getElementById("total_price").innerHTML = grossPrice - minorDiscount
+    document.getElementById("discount").innerHTML = "- \u20AC" + minorDiscount;
+    document.getElementById("total_price").innerHTML = "\u20AC" + (grossPrice - minorDiscount);
 
 } else if (userAge > 65) {
     const overDiscount = ((grossPrice * 40) / 100).toFixed(2);
-    document.getElementById("discount").innerHTML = overDiscount
-    document.getElementById("total_price").innerHTML = grossPrice - overDiscount
+    document.getElementById("discount").innerHTML = "- \u20AC" + overDiscount
+    document.getElementById("total_price").innerHTML = "\u20AC" + (grossPrice - overDiscount)
 
 } else {
     const noDiscount = 0
-    document.getElementById("discount").innerHTML = noDiscount
-    document.getElementById("total_price").innerHTML = grossPrice
+    document.getElementById("discount").innerHTML = "- \u20AC" + noDiscount;
+    document.getElementById("total_price").innerHTML = "\u20AC" + grossPrice
 }
-
