217473e014167f7bc06d1455ff4ee1b732db380c
gbelda <gbelda56@gmail.com>
2021-10-11 14:00:10 +0200

    Written down the rules to apply in JS

---
+++ main.js
@@ -0,0 +1,5 @@
+// chiedere all'utente il numero di chilometri che vuole percorrere
+// chiedere l'eta del passeggero
+//calcolare prezzo del viaggio (0.21 € al km)
+//applicare lo sconto in base all 'eta (20% minorenni, 40% over 65)
+// mostrare il prezzo finale
