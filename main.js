f779cdd89e17e93881a9c979053eba6878668b01
gbelda <gbelda56@gmail.com>
2021-10-11 14:11:19 +0200

    Applied JS calc for base price by kilometres

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 // chiedere all'utente il numero di chilometri che vuole percorrere
+const kilometres = parseInt(prompt("Quanti chilometri vorresti percorrere?"));
+
 // chiedere l'eta del passeggero
+const userAge = parseInt(prompt("Quanti anni ha il passegero?"));
+
 //calcolare prezzo del viaggio (0.21 € al km)
+const grossPrice = kilometres * 0.21
+document.getElementById("initialprice").innerHTML = grossPrice
+
 //applicare lo sconto in base all 'eta (20% minorenni, 40% over 65)
+
 // mostrare il prezzo finale
